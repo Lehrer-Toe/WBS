d5f3e04fd844c010c6d61d653869d72336c7542b
Lehrer-Toe <thomas.toellner@gmx.de>
2025-05-18 13:52:35 +0200

    Create firebaseConfig.js

---
+++ js/firebaseConfig.js
@@ -0,0 +1,9 @@
+// js/firebaseConfig.js
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAMdHLgcVK0G3E6E2a1g1YAo7AEPlHpgnl",
+  authDomain: "rsbs-wbs.firebaseapp.com",
+  projectId: "rsbs-wbs",
+  storageBucket: "rsbs-wbs.appspot.com",
+  messagingSenderId: "301904340569",
+  appId: "1:301904340569:web:XXXXX" // Ersetzen Sie XXXXX mit Ihrer tatsächlichen App-ID
+};
