811ea378c026b5e6418b7c816bb172eab2b2ba51
Lehrer-Toe <thomas.toellner@gmx.de>
2025-05-24 15:44:37 +0200

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,9 +1,9 @@
-// js/firebaseConfig.js
+js/firebaseConfig.js
 export const FIREBASE_CONFIG = {
-  apiKey: "IHRE_FIREBASE_API_KEY",
-  authDomain: "IHRE_FIREBASE_AUTH_DOMAIN",
-  projectId: "IHRE_FIREBASE_PROJECT_ID",
-  storageBucket: "IHRE_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "IHRE_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "IHRE_FIREBASE_APP_ID"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
